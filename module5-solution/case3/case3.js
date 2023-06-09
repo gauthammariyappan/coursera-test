052907872699818ce76fc6e757825e8a15044b9d
gauthammariyappan <60974224+gauthammariyappan@users.noreply.github.com>
2023-06-09 17:11:44 +0530

    Add files via upload

--- module5-solution/case3/case3.js
+++ module5-solution/case3/case3.js
@@ -1,51 +1,41 @@
 function exe(){
     convertUpperCase(10);
-  }
-  
-  function func1(str){
-      func2(str);
-  }
-  
-  function func2(str){
-      func3(str);
-  }
-  
-  function func3(str){
-      func4(str);
-  }
-  
-  function func4(str){
-      func5(str);
-  }
-  
-  function func5(str){
-      func6(str);
-  }
-  
-  function func6(str){
-      func7(str);
-  }
-  
-  function func7(str){
-      func8(str);
-  }
-  
-  function func8(str){
-      func9(str);
-  }
-  
-  function func9(str){
-      convertUpperCase(str);
-  }
-  
-  // Function to find the sum of the array
-  function calculateSum(arr) {
+}  
+function func1(str){
+    func2(str);
+}
+function func2(str){
+    func3(str);
+}  
+function func3(str){
+    func4(str);
+}  
+function func4(str){
+    func5(str);
+} 
+function func5(str){
+    func6(str);
+}  
+function func6(str){
+    func7(str);
+}
+function func7(str){
+    func8(str);
+}  
+function func8(str){
+    func9(str);
+}  
+function func9(str){
+    convertUpperCase(str);
+}
+// Function to find the sum of the array
+function calculateSum(arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
       sum += arr[i];
     }
     return sum;
-  }  
+}
 
 function case3(){
     console.log(myVariable);
