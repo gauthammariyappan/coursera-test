fe521216548856095e33a72861662df98c6dc940
gauthammariyappan <60974224+gauthammariyappan@users.noreply.github.com>
2023-06-09 17:10:28 +0530

    Add files via upload

--- module5-solution/case1/case1.js
+++ module5-solution/case1/case1.js
@@ -1,55 +1,43 @@
 function exe(){
     convertUpperCase(10);
-  }
-  
-  function func1(str){
-      func2(str);
-  }
-  
-  function func2(str){
-      func3(str);
-  }
-  
-  function func3(str){
-      func4(str);
-  }
-  
-  function func4(str){
-      func5(str);
-  }
-  
-  function func5(str){
-      func6(str);
-  }
-  
-  function func6(str){
-      func7(str);
-  }
-  
-  function func7(str){
-      func8(str);
-  }
-  
-  function func8(str){
-      func9(str);
-  }
-  
-  function func9(str){
-      convertUpperCase(str);
-  }
-  
-  // Function to find the sum of the array
-  function calculateSum(arr) {
+}  
+function func1(str){
+    func2(str);
+}
+function func2(str){
+    func3(str);
+}  
+function func3(str){
+    func4(str);
+}  
+function func4(str){
+    func5(str);
+} 
+function func5(str){
+    func6(str);
+}  
+function func6(str){
+    func7(str);
+}
+function func7(str){
+    func8(str);
+}  
+function func8(str){
+    func9(str);
+}  
+function func9(str){
+    convertUpperCase(str);
+}
+// Function to find the sum of the array
+function calculateSum(arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
       sum += arr[i];
     }
     return sum;
-  }
+}
   
-  
-
-function case1() {
-let x= 10;
-x = y + 10;
+function case1(){
+    let x= 10;
+    x = y + 10;
 }
